aa4b8ac723d7ead3283bc25cb34b48fc2a15da40
Jerry Marks <ibjerrymarks7@gmail.com>
2020-06-26 14:27:23 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Github_InClassExercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
